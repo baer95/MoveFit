f5e817b2428415fcbc26d88b35655a4af1bc2695
Internerd <internerd@gmx.de>
2016-09-24 09:24:54 +0200

    FUCK this

--- src/home.ts
+++ src/home.ts
@@ -1,15 +1,26 @@
 import {inject} from "aurelia-framework";
 import {UserDataService} from "services/UserDataService";
 import {TimerService} from "services/TimerService";
+import {EventAggregator} from 'aurelia-event-aggregator';
 
-@inject(UserDataService, TimerService)
+@inject(UserDataService, TimerService, EventAggregator)
 export class Home {
   userDataService;
   timerService;
+  eventAggregator;
 
-  constructor(userDataService, timerService){
+  activitySubscriber;
+
+  constructor(userDataService, timerService, eventAggregator){
 
     this.userDataService = userDataService;
     this.timerService = timerService;
+    this.eventAggregator = eventAggregator;
+  }
+
+  private subscribeToNoGps(){
+    this.activitySubscriber = this.eventAggregator.subscribe("activityChannel", nogps => {
+      console.log(nogps);
+    });
   }
 }
